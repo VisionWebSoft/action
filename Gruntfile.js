f561b95f9cd9667f8d2bb24574006c169016a3ac
manavm1990 <manavm@visionwebsoft.com>
2015-12-05 18:49:17 -0600

    Prevent concatentation of 'vendor' CSS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,8 +55,7 @@ module.exports = function( grunt ) { // just a node thing
     },
     concat: {
       concat: {
-        src: [ 'src/css/**/*.css', '!src/css/grid.min.css',
-        '!src/css/bootstrap.min.css'],
+        src: [ 'src/css/*.css' ],
         dest: 'build/css/action.min.css' // Not actually minified.
       }
     },
